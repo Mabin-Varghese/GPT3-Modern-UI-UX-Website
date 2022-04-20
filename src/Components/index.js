ad823b96df25242f15cfc3fd44553895bbf6f5fb
Mabin-Varghese <mabinvarghese23@gmail.com>
2022-04-20 15:16:45 +0530

    Add : Components and containers folders added and also NavBar created

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as Article} from './Articles/Article'
+export { default as Brand} from './Brand/Brand'
+export { default as CTA} from './CTA/CTA'
+export { default as Feature} from './Features/Feature'
+export { default as NavBar} from './NavBar/NavBar'
