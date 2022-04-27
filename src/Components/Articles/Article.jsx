a35cf16221a7d84170d28d568f0dabfca464a5c3
Mabin-Varghese <mabinvarghese23@gmail.com>
2022-04-27 22:15:30 +0530

    Add : Blog section added

--- src/Components/Articles/Article.jsx
+++ src/Components/Articles/Article.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './article.css'
+import React from "react";
+import "./article.css";
 
-const Article = () => {
-  return (
-    <div>Article</div>
-  )
-}
+const Article = ({ imgUrl , date, title }) => {
+    return (
+        <div className="gpt3__blog-cointainer_ariticle">
+            <div className="gpt3__blog-cointainer_ariticle_image">
+                <img src={imgUrl} alt="Blog" />
+            </div>
+            <div className="gpt3__blog-cointainer_ariticle-content">
+              <div>
+              <p>{date}</p>
+              <h3>{title}</h3>
+              </div>
+              <p>Read Full Article</p>
+            </div>
+        </div>
+    );
+};
 
-export default Article
+export default Article;
